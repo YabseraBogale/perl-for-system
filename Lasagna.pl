a9595bf7f43bc20eb9c498b8e2a38bb650ed3beb
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-30 20:21:42 +0300

    Lasagna.pl

---
+++ Lasagna.pl
@@ -0,0 +1,21 @@
+package Lasagna;
+
+use v5.34.0;
+
+my $ExpectedMinutesInOven = 40;
+
+sub remaining_minutes_in_oven ($actual_minutes_in_oven) {
+    return $ExpectedMinutesInOven-$actual_minutes_in_oven;
+}
+
+sub preparation_time_in_minutes ($number_of_layers) {
+    return $number_of_layers*2;
+}
+
+sub total_time_in_minutes ( $number_of_layers, $actual_minutes_in_oven ) {
+    return $actual_minutes_in_oven+(2*$number_of_layers);
+}
+
+sub oven_alarm () {
+	return "Ding!";
+}
